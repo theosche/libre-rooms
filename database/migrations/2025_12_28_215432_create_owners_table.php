3ef63ff4ccf80cd579550de8b2edc7d0d0783251
Théophile Schenker <theophile@MacBook-Pro-de-Theophile.local>
2026-02-01 21:55:53 +0100

    merge migrations

--- database/migrations/2025_12_28_215432_create_owners_table.php
+++ database/migrations/2025_12_28_215432_create_owners_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('owners', function (Blueprint $table) {
             $table->id();
             $table->string('slug')->unique();
+            $table->string('website')->nullable();
             $table->foreignId('contact_id')->constrained()->ondelete('cascade');
 
             $table->string('invoice_due_mode');
